169f17a5b965dbda91155460ec67934fea72af75
ellnix <ellnix@disroot.org>
2024-09-02 09:50:12 +0200

    Move state up to the parent
    The preferred way to synchronize multiple components
    - Uses immutability in order to allow components to cheaply compare
      state and check if they should be re-rendered

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import { useState } from 'react'
 
-function Square() {
-  const [squareValue, setSquare] = useState(null)
-  
-  function tickSquare() {
-    setSquare('X')
-  }
-
+function Square({label, onSquareClick}) {
   return (
     <button 
       className="square" 
-      onClick={tickSquare}
+      onClick={onSquareClick}
     >
-      {squareValue}
+      {label}
     </button>
   )
 }
 export default function Board() {
+  const [nextPlayer, setNextPlayer] = useState('X')
+  const [squares, setSquares] = useState(new Array(9).fill(null))
+
+  function clickSquare(square) {
+    if (squares[square] !== null) return
+
+    const nextSquares = squares.slice()
+    nextSquares[square] = nextPlayer
+    setNextPlayer(nextPlayer == 'X' ? 'O' : 'X')
+    setSquares(nextSquares)
+  }
+
   return(
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square label={squares[0]} onSquareClick={() => clickSquare(0)} />
+        <Square label={squares[1]} onSquareClick={() => clickSquare(1)} />
+        <Square label={squares[2]} onSquareClick={() => clickSquare(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square label={squares[3]} onSquareClick={() => clickSquare(3)} />
+        <Square label={squares[4]} onSquareClick={() => clickSquare(4)} />
+        <Square label={squares[5]} onSquareClick={() => clickSquare(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square label={squares[6]} onSquareClick={() => clickSquare(6)} />
+        <Square label={squares[7]} onSquareClick={() => clickSquare(7)} />
+        <Square label={squares[8]} onSquareClick={() => clickSquare(8)} />
       </div>
     </>
   )
